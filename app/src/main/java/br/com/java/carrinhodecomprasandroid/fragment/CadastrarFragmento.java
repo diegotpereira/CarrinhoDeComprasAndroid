f4340de85a02fea4af91f7c5fdc9783e031ca4ca
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-05-24 23:27:02 -0300

    criação do arquivo fragmento.

--- app/src/main/java/br/com/java/carrinhodecomprasandroid/fragment/CadastrarFragmento.java
+++ app/src/main/java/br/com/java/carrinhodecomprasandroid/fragment/CadastrarFragmento.java
@@ -2,6 +2,8 @@ package br.com.java.carrinhodecomprasandroid.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -27,5 +29,10 @@ public class CadastrarFragmento extends Fragment {
         View view=inflater.inflate(R.layout.fragmento_cadastrar, container, false);
         parentFrameLayout=getActivity().findViewById(R.id.registrar_framelayout);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
